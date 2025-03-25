8548689eec5b60487eee0c3228caaa6e589aa95d
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-25 15:28:56 +0600

    Create dart_map.dart

---
+++ dart_map.dart
@@ -0,0 +1,12 @@
+void main() {
+  var parson = {
+    'name' : 'samin'  ,
+     'city' : 'ctg',
+      'age' : 50 ,
+       'height' : '5 foor 7 inch'
+
+  } ;
+
+  print(parson);
+  print(parson['city']) ;
+}

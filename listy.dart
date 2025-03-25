134edab02a130ad48fcaa4620f8604b145505f30
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-25 15:43:53 +0600

    Create listy.dart

---
+++ listy.dart
@@ -0,0 +1,20 @@
+void main() {
+   var  city = ['ctg' , 'rajishai' , 'bogura' , 'dhaka'] ;
+
+   var size = city.length;
+   var proth =  city.first;
+   var seshh =  city.last;
+
+   var rev = city.reversed;
+ //  var simng = city.single; only 1 item
+   var emm = city.isEmpty;
+   var emmno = city.isNotEmpty;
+   print(size);
+   print(proth);
+   print(seshh);
+   print(rev);
+   print(emm);
+   print(emmno);
+
+
+}

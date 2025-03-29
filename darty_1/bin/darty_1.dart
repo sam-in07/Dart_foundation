5593791b553607f1a653fda222c5fe1bf0721cd3
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-30 04:59:54 +0600

    acees

---
+++ darty_1/bin/darty_1.dart
@@ -0,0 +1,17 @@
+import 'myclass.dart';
+
+
+void main() {
+  var obj = new Myclass();
+  obj.addtwo(20, 29);
+  obj.addthree(33, 45, 89) ;
+
+
+  print(obj.myname);
+
+  print(obj.Alphabe);
+  print(obj.Alphabe[2]);
+
+
+
+}
---
+++ darty_1/bin/myclass.dart
@@ -0,0 +1,10 @@
+class Myclass{
+  var myname = "samin ahmed" ;
+  var Alphabe = ['a', 'b' , 'c' , 'd'];
+  addtwo(int x , int y) {
+    print(x+y);
+  }
+  addthree(int x , int  y , int z) {
+    print(x+y+z);
+  }
+}

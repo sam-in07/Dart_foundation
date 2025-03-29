3e40eafbee99b03e982a119177701251bf40a90b
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-29 06:33:32 +0600

    Create map_func.dart

---
+++ map_func.dart
@@ -0,0 +1,16 @@
+void main() {
+  var person = new Map();
+
+  person['name'] = 'samin' ;
+  person['age'] = 20 ;
+  person['city'] = 'Dhalka' ;
+
+  print(person);
+  print(person.keys);
+  print(person.values);
+  print(person.length);
+  print(person.isEmpty);
+
+
+
+}

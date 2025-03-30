6f2b76b3b9308d33ca93064e8f8eff782417e765
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-30 06:01:00 +0600

    Create inheritance.dart

---
+++ inheritance.dart
@@ -0,0 +1,15 @@
+class Father {
+  void baperTaka() {
+    print("Total Amount = 80000000");
+  }
+}
+
+class Son extends Father {}
+
+void main() {
+  Father fatherObj = Father();
+  fatherObj.baperTaka();
+
+  Son sonObj = Son();
+  sonObj.baperTaka();
+}
